e123913f5227cef768b326da8f87ee5d9ba30404
Jabar Mohammadi <jabar.mohammadi.dev@gmail.com>
2022-08-06 23:43:08 +0430

    get data and insert in candle struct

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type binanceData [][]interface{}
@@ -20,8 +22,24 @@ type candle struct {
 	Volume             float64
 	UpperShadowPercent float64
 	DownShadowPercent  float64
-	OpenTime           int32
-	CloseTime          int32
+	OpenTime           int
+	CloseTime          int
+}
+
+func convertToFloat64(s string) float64 {
+	f, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		log.Println(err)
+	}
+	return f
+}
+
+func convertToInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		log.Println(err)
+	}
+	return i
 }
 
 func main() {
@@ -39,12 +57,28 @@ func main() {
 
 	err = json.Unmarshal(body, &data)
 
+	d := json.NewDecoder(strings.NewReader(string(body)))
+	d.UseNumber()
+	if err := d.Decode(&data); err != nil {
+		log.Fatal(err)
+	}
+
+	// fmt.Println(data)
+
 	if err != nil {
 		log.Println(err)
 	}
 
 	for _, v := range data {
-		fmt.Println(v)
+		var candle candle
+		candle.OpenTime = convertToInt(fmt.Sprint(v[0])) 
+		candle.Open = convertToFloat64(fmt.Sprint(v[1]))
+		candle.High = convertToFloat64(fmt.Sprint(v[2]))
+		candle.Low = convertToFloat64(fmt.Sprint(v[3]))
+		candle.Close = convertToFloat64(fmt.Sprint(v[4]))
+		candle.Volume = convertToFloat64(fmt.Sprint(v[5]))
+		candle.CloseTime = convertToInt(fmt.Sprint(v[6]))
+		fmt.Println(candle)
 	}
 
 }
